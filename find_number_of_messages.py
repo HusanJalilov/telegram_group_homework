a910745d0204200ea5f939890d80f65e587b0254
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2022-02-02 02:16:36 +0500

    TAsk 03

--- find_number_of_messages.py
+++ find_number_of_messages.py
@@ -1,5 +1,5 @@
 from read_data import read_data
-
+import json
 def find_number_of_messages(data: dict)->int:
     """
     Get the total number of messages.
@@ -11,3 +11,7 @@ def find_number_of_messages(data: dict)->int:
     
     """
     return len(data['messages'])
+
+f=open('result.json',encoding='utf8').read()
+data=json.loads(f)
+print(find_number_of_messages(data))
