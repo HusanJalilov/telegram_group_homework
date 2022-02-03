55ea921169ea8cdad20e98ef4904e7d727e98c5a
Husan <husanjalilov00777@gmail.com>
2022-02-03 10:03:05 +0500

    users id

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -16,7 +16,7 @@ def find_all_users_id(data: dict)->list:
     itm=[]
     col=data["messages"]
     for x in range(len(col)):        
-        ser=col[x].get('actor_id')
+        ser=col[x].get('actor_id',False)
         if ser:
             s.append(ser)
     
